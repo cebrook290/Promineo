b0c2b36b58301246af1132261f484701e258212b
Chris Esterbrook <cebrook290@gmail.com>
2022-12-06 20:10:29 -0800

    week 1 examples during class 11-29

---
+++ Week1Class/script.js
@@ -0,0 +1,33 @@
+//print hello\
+console.log('hello');
+
+//variable named x equal to 5
+let x = 5;
+
+//print variable x
+console.log(x);
+
+//reassign x to 7
+x = 7;
+var y = 3;
+var z = x + 3;
+
+console.log(z);
+
+let b = x * y;
+console.log(b);
+
+b++;
+console.log(b);
+
+b--;
+console.log(b);
+
+
+b = b % 3;
+console.log(b);
+
+b = 21;
+
+b = b % 2;
+console.log(b);
